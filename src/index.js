7c31ab3a4b125623d4955489e4fd691c45869ad7
Marta Kule <mkule@mkule-mac.local>
2022-05-04 13:23:40 -0400

    add local theme - does not work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { lightTheme, Provider } from "@adobe/react-spectrum";
+// import { lightTheme, Provider } from "@adobe/react-spectrum";
+import { Provider } from "@adobe/react-spectrum";
+import { theme } from "./theme/customTheme";
 import "./index.css";
 import SignIn from "./SignIn";
 import ForgotPassword from "./ForgotPassword";
@@ -9,7 +11,8 @@ import Success from "./Success";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider theme={lightTheme}>
+    {/* <Provider theme={lightTheme}> */}
+    <Provider theme={theme}>
       <BrowserRouter basename="/spectrum-hackathon">
         <Routes>
           <Route path="/" element={<SignIn />} />
---
+++ src/theme/customTheme.js
@@ -0,0 +1,13 @@
+import global from "./global.css";
+import light from "./light.css";
+import dark from "./dark.css";
+import medium from "./medium.css";
+import large from "./large.css";
+
+export let theme = {
+  global,
+  light,
+  dark,
+  medium,
+  large,
+};
